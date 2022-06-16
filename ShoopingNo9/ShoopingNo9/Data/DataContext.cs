e64d335e18be252a7a9cdd262b52f6215fc580af
antonio lopez <alopez1095@hotmail.com>
2022-06-16 13:36:25 -0600

    CRUDCountryStateCity

---
+++ ShoopingNo9/ShoopingNo9/Data/DataContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShoopingNo9.Data.Entities;
+
+namespace ShoopingNo9.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> option) : base(option)
+        {
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<City> Cities { get; set; }
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<State> States { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Category>().HasIndex(c => c.Name).IsUnique();
+            modelBuilder.Entity<Country>().HasIndex(c => c.Name).IsUnique();
+            modelBuilder.Entity<City>().HasIndex("Name","StateId").IsUnique();
+            modelBuilder.Entity<State>().HasIndex("Name", "CountryId").IsUnique();
+        }
+    }
+}
