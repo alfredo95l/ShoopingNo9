f258f212715cb989b15268acff7810ace915f53f
antonio lopez <alopez1095@hotmail.com>
2022-07-06 23:35:12 -0600

    IniciamosPruebas

---
+++ ShoopingNo9/ShoopingNo9/Migrations/20220707025301_InitproductNO4.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShoopingNo9.Migrations
+{
+    public partial class InitproductNO4 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ ShoopingNo9/ShoopingNo9/Migrations/20220707051120_Uno.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShoopingNo9.Migrations
+{
+    public partial class Uno : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
